60f33e50e79b09a7c963bc2266089cd82592dae3
Fábio Santos <fabiosantos@ifsul.edu.br>
2025-03-13 07:58:20 -0300

    Definição de Classes e Objetos - Namespaces

--- 01-classes-objetos/index.php
+++ 01-classes-objetos/index.php
@@ -1,10 +1,15 @@
 <?php
-
-echo "Olá, Mundo da Orientação a Objetos em PHP!";
-
 require __DIR__ . "/../source/Models/User.php";
+
+echo "Olá, Mundo da Orientação a Objetos em PHP!<br>";
+
+//$user = new \Source\Models\User();
+
 use Source\Models\User;
-
-$user = new User();
-
+$user = new User(12, "Godofredo" );
 var_dump($user);
+
+/*$user->id = 12;
+$user->name = "Fábio";*/
+//var_dump($user);
+//$user->login();
--- source/Models/User.php
+++ source/Models/User.php
@@ -5,4 +5,16 @@ namespace Source\Models;
 class User
 {
     private $id;
+    private $name;
+
+    public function __construct (int $id, string $name)
+    {
+        //echo "Olá, {$name} seu tipo é {$type}";
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    public function login () {
+        echo "Olá, {$this->name}! Você por aqui de novo?";
+    }
 }
