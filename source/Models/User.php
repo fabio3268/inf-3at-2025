2200901bf3787266c7be91e4392b8b4d974ef70a
Fábio Santos <fabiosantos@ifsul.edu.br>
2025-03-18 14:33:56 -0300

    Namaspaces

--- source/Models/User.php
+++ source/Models/User.php
@@ -5,33 +5,90 @@ namespace Source\Models;
 class User
 {
     private $id;
+    private $idType;
     private $name;
+    private $email;
+    private $password;
+    private $photo;
 
-    public function __construct (int $id = null, string $name = null)
+    public function __construct(
+        int $id = null,
+        int $idType = null,
+        string $name = null,
+        string $email = null,
+        string $password = null,
+        string $photo = null)
     {
         $this->id = $id;
+        $this->idType = $idType;
         $this->name = $name;
+        $this->email = $email;
+        $this->password = $password;
+        $this->photo = $photo;
     }
 
-    public function getId (): ?int
+    public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function setId (?int $id): void
+    public function setId(?int $id): void
     {
         $this->id = $id;
     }
 
-    public function getName (): ?string
+    public function getIdType(): ?int
+    {
+        return $this->idType;
+    }
+
+    public function setIdType(?int $idType): void
+    {
+        $this->idType = $idType;
+    }
+
+    public function getName(): ?string
     {
         return $this->name;
     }
 
-    public function setName (?string $name): void
+    public function setName(?string $name): void
     {
         $this->name = $name;
     }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(?string $password): void
+    {
+        $this->password = $password;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(?string $photo): void
+    {
+        $this->photo = $photo;
+    }
+
+
+
 
     public function login () {
         echo "Olá, {$this->name}! Você por aqui de novo?";
