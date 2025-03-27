8d32dbabf4b21f98bc9acb008065b2d73a3cab3c
Fábio Santos <fabiosantos@ifsul.edu.br>
2025-03-27 17:52:33 -0300

    Acessando Banco de Dados com a PDO

--- 07-pdo/index.php
+++ 07-pdo/index.php
@@ -1,23 +1,5 @@
 <?php
 
-/*$dbUser = "root";
-$dbPassword = "asdf1234"; // sem senha para vocês
-$options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
-];
-
-// para vocês é host=localhost
-$connection = new PDO(
-    "mysql:host=mysql;dbname=db-inf-3at",
-    $dbUser,
-    $dbPassword,
-    $options
-);
-
-var_dump($connection);*/
-
-
 require __DIR__ . "/../source/autoload.php";
 
 use Source\Models\User;
